c1154565dcc5deab8df8dc4f524524579e9f0225
TinTurtle <sivagaminitish@gmail.com>
2025-12-30 23:32:30 +0530

    fixed bug

--- kilo.c
+++ kilo.c
@@ -265,7 +265,7 @@ void editorFreeRow(erow *row){
 void editorDelRow(int at){
     if(at<0 && at>=E.numrows)return;
     editorFreeRow(&E.row[at]);
-    memmove(&E.row[at], &E.row[at+1], sizeof(erow) * (E.numrows - at + 1));
+    memmove(&E.row[at], &E.row[at+1], sizeof(erow) * (E.numrows - at - 1));
     E.numrows--;
     E.dirty++;
 }
