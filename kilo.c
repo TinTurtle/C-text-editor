931362f87c94730b4a86417190c6a59c5b070bd8
TinTurtle <sivagaminitish@gmail.com>
2026-01-10 17:24:49 +0530

    save as feature added for new unnamed file

--- kilo.c
+++ kilo.c
@@ -78,6 +78,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
+char *editorPrompt(char  *prompt);
 
 /*** terminal ***/
 
@@ -316,7 +317,7 @@ void editorInsertNewLine(){
         editorInsertRow(E.cy, "", 0);
     }else{
         erow *row = &E.row[E.cy];
-        editorInsertRow(E.cy, &row->chars[E.cx], row->size - E.cx);
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
         row = &E.row[E.cy];
         row->size = E.cx;
         row->chars[row->size] = '\0';
@@ -386,7 +387,13 @@ char *editorRowsToString(int *buflen){
 
 
 void editorSave(){
-	if(E.filename == NULL) return;
+	if(E.filename == NULL){
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if(E.filename == NULL){
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
 
 	int len;
 	char *buf = editorRowsToString(&len);
@@ -567,12 +574,20 @@ char *editorPrompt(char *prompt){
         editorRefreshScreen();
         
         int c = editorReadKey();
-        if(c == '\r'){
+        if(c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE){
+            if(buflen != 0){
+                buf[--buflen] = '\0';
+            }
+        } else if(c == '\x1b'){
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        } else if(c == '\r'){
             if(buflen != 0){
                 editorSetStatusMessage("");
                 return buf;
             }
-        }else if(!iscntrl(c) && c<128){
+        } else if(!iscntrl(c) && c<128){
             if(buflen == bufsize - 1){
                 bufsize = 2;
                 buf = realloc(buf, bufsize);
